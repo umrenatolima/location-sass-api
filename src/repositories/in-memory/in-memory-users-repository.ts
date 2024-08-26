fa39f42ceff4c77fa64ba54a439bb50becfa7ecf
Renato Lima <renatoaugustoml@gmail.com>
2024-08-26 07:57:14 +0200

    feat: add findById method to users repository

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -25,4 +25,12 @@ export class InMemoryUsersRepository implements UsersRepository {
 
     return user;
   }
+
+  async findById(id: string) {
+    const user = this.users.find((item) => item.id === id);
+
+    if (!user) return null;
+
+    return user;
+  }
 }
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -14,4 +14,11 @@ export class PrismaUsersRepository implements UsersRepository {
     });
     return user;
   }
+
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+    return user;
+  }
 }
